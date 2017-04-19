85ee0391441d2b92e9ddaff99c28d53e50bbe2b4
xiejianan <xiejianan86@gmail.com>
2017-04-19 16:53:51 +0800

    hide

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,6 +11,10 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] ="this job already archieved"
+      redirect_to root_path
+    end
   end
 
   def create
--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,9 +2,9 @@ module JobsHelper
 
   def render_job_status(job)
     if job.is_hidden
-      "(hidden)"
+      content_tag(:span,"",:class => "fa fa-lock")
     else
-      "(public)"
+      content_tag(:span,"",:class => "fa fa-globe")
     end
   end
 end
